71386766a874de1e753c17825b01ab5c80e2fbaf
Prince Kumar <prince18pat@gmail.com>
2015-10-26 23:13:56 +0900

    Using angular js controller

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -1,8 +1,3 @@
-/*! angular-app - v1.0.0 - 2015-10-26
- * http://princekr.com
- * Copyright (c) 2015  Prince;
- * Licensed 
- */
 var app = angular.module('app',[]);
 
 app.controller('AppCtrl', function($scope){
