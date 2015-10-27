ba2b18189b671cfb6321d85d6c0fdcd28e135416
Prince Kumar <prince18pat@gmail.com>
2015-10-28 00:28:36 +0900

    updating the templates

--- client/dist/angular-app.js
+++ client/dist/angular-app.js
@@ -1,9 +1,17 @@
-/*! angular-app - v1.0.0 - 2015-10-27
+/*! angular-app - v1.0.0 - 2015-10-28
  * http://princekr.com
  * Copyright (c) 2015  Prince;
  * Licensed 
  */
-angular.module('app',[]);
+angular.module('app',['ngRoute','templates.app','templates.common']);
+
+angular.module('app').constant('I18N.MESSAGES', {
+  'errors.route.changeError': 'Route change error'
+});
+
+//angular.module('app').config(['$routeProvider', function($routeProvider){
+  //$routeProvider.otherwise({ redirectTo: '/projectsinfo'});
+//}]);
 
 angular.module('app').controller('AppCtrl', ['$scope',function($scope){
 
@@ -18,9 +26,10 @@ angular.module('app').controller('AppCtrl', ['$scope',function($scope){
   });
 }]);
 
-angular.module('app').controller('HeaderCtrl',['$scope', function($scope){
+angular.module('app').controller('HeaderCtrl',['$scope', '$location',
+  function($scope,$location){
 
-  $scope.title = "Header";
+  $scope.location = $location;
 
 }]);
 
@@ -37,7 +46,7 @@ angular.module('templates.app', ['header.tpl.html', 'notifications.tpl.html']);
 angular.module("header.tpl.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("header.tpl.html",
     "<div class=\"navbar\" ng-controller=\"HeaderCtrl\">\n" +
-    "  Header\n" +
+    "  This is header area\n" +
     "</div>\n" +
     "");
 }]);
@@ -45,7 +54,7 @@ angular.module("header.tpl.html", []).run(["$templateCache", function($templateC
 angular.module("notifications.tpl.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("notifications.tpl.html",
     "<div>\n" +
-    "  Notifications\n" +
+    "  This is notification bar\n" +
     "</div>\n" +
     "");
 }]);
--- client/src/app/app.js
+++ client/src/app/app.js
@@ -1,4 +1,12 @@
-angular.module('app',[]);
+angular.module('app',['ngRoute','templates.app','templates.common']);
+
+angular.module('app').constant('I18N.MESSAGES', {
+  'errors.route.changeError': 'Route change error'
+});
+
+//angular.module('app').config(['$routeProvider', function($routeProvider){
+  //$routeProvider.otherwise({ redirectTo: '/projectsinfo'});
+//}]);
 
 angular.module('app').controller('AppCtrl', ['$scope',function($scope){
 
@@ -13,8 +21,9 @@ angular.module('app').controller('AppCtrl', ['$scope',function($scope){
   });
 }]);
 
-angular.module('app').controller('HeaderCtrl',['$scope', function($scope){
+angular.module('app').controller('HeaderCtrl',['$scope', '$location',
+  function($scope,$location){
 
-  $scope.title = "Header";
+  $scope.location = $location;
 
 }]);
