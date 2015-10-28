5f51cbd9a4e60d5a4a14214ef8eebb5e0c48cc2d
Prince Kumar <prince18pat@gmail.com>
2015-10-28 21:23:31 +0900

    updating certificate

--- server/config.js
+++ server/config.js
@@ -2,10 +2,21 @@ var path = require('path');
 
 module.exports = {
 
+    mongo: {
+      dbUrl: 'https://api.mongolab.com/api/1',
+      apiKey: '4fb44bb44bb44bbbb44bb44'
+      
+    },
+  security: {
+    dbName: 'ascrum',
+    usersCollection: 'users'
+  },
   server: {
     listenPort: 3000,
+    securePort: 8433,
     distFolder: path.resolve(__dirname, '../client/dist'),
     staticUrl: '/static',
+    cookieSecret: 'angular-app'
   }
 
 };
--- server/server.js
+++ server/server.js
@@ -1,18 +1,25 @@
+var fs = require('fs');
 var express = require('express');
 var http = require('http');
 var https = require('https');
+
+var privatekey = fs.readFileSync(__dirname + '/cert/privatekey.pem').toString();
+var certificate = fs.readFileSync(__dirname + '/cert/certificate.pem').toString();
+var credentials = {key: privatekey, cert: certificate};
 
 var config = require('./config.js');
 
 var app = express();
 var server = http.createServer(app);
+var sercureServer = https.createServer(credentials,app);
+
 require('./lib/routes/static').addRoutes(app, config);
 
 app.use(function(req,res,next){
   if( req.user ){
     console.log('Current user: ', req.user.firstName, req.user.lastName);
   }else{
-    console.log('*************');
+    console.log('____________________');
   }
   next();
 });
@@ -23,4 +30,8 @@ app.all('/',function(req,res, next){
 server.listen(config.server.listenPort, '0.0.0.0', 511, function(){
   var open = require('open');
   open('http://localhost:'+ config.server.listenPort+'/');
-})
+});
+
+console.log('Angular app server - listening on port: '+ config.server.listenPort);
+sercureServer.listen(config.server.securePort);
+console.log('Angular App server - listening on secure port: '+ config.server.securePort)
